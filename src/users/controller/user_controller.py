462e23b7d2cc0fec21927265252805b026178063
Danilo Barolo Martins de Lima <dangeschichte@gmail.com>
2026-04-12 01:15:01 +0200

    feat: add delete user endpoint

--- src/users/controller/user_controller.py
+++ src/users/controller/user_controller.py
@@ -50,3 +50,13 @@ async def get_all_users(request: Request, db: DbSession) -> list[UserResponse]:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"{e}"
         )
+
+
+@user_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(request: Request, id: UUID, db: DbSession) -> None:
+    try:
+        user_service.delete_user(id, db)
+    except NoResultFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
